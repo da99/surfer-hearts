b534c4a1067d054cde947095106cb8a9e06edab6
da99 <sp.03+github.spam@miniuni.com>
2020-11-24 04:38:08 -0600

    typos for cloudflare configuration.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  let x = "<html><head><title>Coming soon...</title></head><body>Come back next week.</body></html>";
+  let x = "<html><head><title>Come back in a few days...</title></head><body>Not ready. []]]</body></html>";
   return new Response(x, {
     headers: { 'content-type': 'text/html' },
     status: 200
